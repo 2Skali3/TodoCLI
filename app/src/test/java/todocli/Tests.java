3a4eb643295d0ab9798ef4d84676868e12b68ea2
2Skali3 <130842781+2Skali3@users.noreply.github.com>
2025-04-27 22:21:18 +0200

    Renamed the file FilemanagerTest.java -> Tests.java and created TestPrintTasks() and all works

--- app/src/test/java/todocli/Tests.java
+++ app/src/test/java/todocli/Tests.java
@@ -6,13 +6,25 @@ import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 
-public class FileManagerTest {
+public class Tests {
 
     private final FileManager fileManager = new FileManager();
     
     @Test void TestSavingToFile(){
 
         this.fileManager.saveToFile("Hello There");
+
+    }
+
+    @Test void TestPrintTasks(){
+
+        final Task_Impl tasks = new Task_Impl();
+
+        tasks.AddTask("AAA");
+        tasks.AddTask("BBB");
+        tasks.AddTask("CCC");
+        
+        tasks.PrintTasks();
 
     }
 }
